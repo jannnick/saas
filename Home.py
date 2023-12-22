86d132c91c276f0bc5c9bbcaab838699f9d497bd
jannnick <microyahoo@gmail.com>
2023-12-23 07:59:19 +1300

    Update Home.py

--- Home.py
+++ Home.py
@@ -32,7 +32,10 @@ if 'verified' not in st.session_state:
 
 # Handle login if not authenticated and not verified
 if not st.session_state['authentication_status'] and not st.session_state['verified']:
-    st.session_state['authenticator'].login('Login', 'main')
+    authenticator_obj = st.session_state['authenticator'].login('Login', 'main')
+    if authenticator_obj is not None:
+        name, authentication_status, username = authenticator_obj.login('Login', 'main')
+
 if 'summarized_text' not in st.session_state:
     st.session_state['summarized_text'] = ''
 if 'translation' not in st.session_state:
