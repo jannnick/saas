3e7a3bd10c1eca3a6a2706370f627e9c76713fd5
jannnick <microyahoo@gmail.com>
2023-12-23 07:43:56 +1300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,9 @@
 mkdir -p ~/.streamlit/
+
+# Check if the key exists, if not, initialize it
+if 'authenticator' not in st.session_state:
+    st.session_state.authenticator = None  # Or initialize it with an appropriate value
+
 echo "\
 [general]\n\
 email = \"your-email@domain.com\"\n\
